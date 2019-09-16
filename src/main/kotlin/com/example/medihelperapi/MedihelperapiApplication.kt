042d7fec2142f5b4d12fd7ad9131ca1cc03fa2cc
maruchin <modelarstwo1>
2019-09-16 22:42:10 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/medihelperapi/MedihelperapiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.medihelperapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedihelperapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedihelperapiApplication>(*args)
+}
