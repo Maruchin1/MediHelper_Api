f4e9659df320fc67316697ee126d4c11f6471938
mapiekielny <mpiekielny@fream.pl>
2020-03-28 11:54:33 +0100

    Dodanie konfiguracji CORS w kontrolerach

--- src/main/kotlin/com/example/medihelperapi/controller/ChildrenController.kt
+++ src/main/kotlin/com/example/medihelperapi/controller/ChildrenController.kt
@@ -11,21 +11,25 @@ class ChildrenController(
     private val childrenService: ChildrenService
 ) {
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @PostMapping("children/login")
     fun loginChild(@RequestBody loginChildDto: LoginChildDto): String {
         return childrenService.login(loginChildDto)
     }
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @PostMapping("/children")
     fun addNewChild(@RequestBody postChildDto: PostChildDto) {
         childrenService.addNew(postChildDto)
     }
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @GetMapping("/children")
     fun getAllChildren(): List<GetChildDto> {
         return childrenService.getAll()
     }
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @DeleteMapping("/children/{id}")
     fun deleteChild(@PathVariable("id") id: Long) {
         childrenService.delete(id)
--- src/main/kotlin/com/example/medihelperapi/controller/ParentsController.kt
+++ src/main/kotlin/com/example/medihelperapi/controller/ParentsController.kt
@@ -3,6 +3,7 @@ package com.example.medihelperapi.controller
 import com.example.medihelperapi.dto.LoginParentDto
 import com.example.medihelperapi.dto.RegisterParentDto
 import com.example.medihelperapi.service.ParentsService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,11 +13,13 @@ class ParentsController(
     private val parentsService: ParentsService
 ) {
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @PostMapping("/parents/register")
     fun registerParent(@RequestBody registerParentDto: RegisterParentDto): String {
         return parentsService.register(registerParentDto)
     }
 
+    @CrossOrigin(origins = ["http://localhost:4200"])
     @PostMapping("/parents/login")
     fun loginParent(@RequestBody loginParentDto: LoginParentDto): String {
         return parentsService.login(loginParentDto)
